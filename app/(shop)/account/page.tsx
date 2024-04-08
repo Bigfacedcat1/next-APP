ab2fd09d295d6a4e67763db4e1c52deee1f7dccc
luopeng <1484221232@qq.com>
2024-04-08 16:19:31 +0800

    路由

---
+++ app/(shop)/account/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+  <h1 className={'text-4xl text-pink-400'}>Hello, account!</h1>
+  )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+  <h1 className={'text-4xl text-orange-400'}>Hello, Next.js!</h1>
+  )
+}
